bd5ccd361b118dd936e1b1a542ea3f81b9d9ee7f
Rebeca-desen <rebecarss01@gmail.com>
2020-12-07 15:19:12 -0300

    teste nas rotas de contas ok, algumas observações para ver depois. Integração com o mongodb atlas também ok

--- src/model/contas.js
+++ src/model/contas.js
@@ -4,7 +4,7 @@ const contasSchema = new mongoose.Schema({
     donxDaConta: { type: String, required: true },
     conta: { type: String, required: true },
     tipoDeConta: { type: String, required: true },
-    dadosDaConta: { type: Array, required: true }
+    dadosDaConta: { type: Array, required: true } // no banco de dados fica salvo como array
 },
 {versionKey: false}
 )
