ad62f8a20b04420f363ded879f57c3b957289096
Rebeca-desen <rebecarss01@gmail.com>
2020-12-07 14:01:34 -0300

    ajuste no index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 router.get('/', function(req, res) {
     res.status(200).send({
       "TITLE":  "DIÁRIO DE SENHAS",
-      "subtitle": "Um lugar seguro para você armazenar e organizar suas senhas",
+      "subtitle": "Um lugar seguro e fácil para você armazenar e organizar suas senhas",
       "version": "1.0.0"
     })
 })
