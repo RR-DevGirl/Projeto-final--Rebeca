c7de08474a5c1a3c01b1735ecbb9edc04709e93a
Rebeca-desen <rebecarss01@gmail.com>
2020-12-07 15:56:10 -0300

    ajuste na rota

--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/usuarioController')
 
-router.post('/', controller.create)
+router.post('/cadastro', controller.create)
 
 module.exports = router
