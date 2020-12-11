e04d97388df83ef0ce38e09e3386efe53e95feb6
Rebeca-desen <rebecarss01@gmail.com>
2020-12-11 19:03:16 -0300

    autenticações okay

--- src/routes/contasRoute.js
+++ src/routes/contasRoute.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/contasController')
+const jwt = require('jsonwebtoken')
+const SECRET = process.env.SECRET
+
+
+const autenticar = (req, res, next) => {
+    const authHeader = req.get('authorization')
+
+    if(!authHeader){ return res.status(401).send('cadê os heards anjo')}
+  
+    const token = authHeader.split(' ')[1]
+  
+  
+    jwt.verify(token, SECRET, function(erro){
+       
+      if(erro){
+        
+       return res.status(403).send('foi não visse? tem algo errado')
+      }
+
+  next()
+})
+}
 
 router.get('/dev', controller.getAllDev)
-router.get('/:tipoDeConta', controller.tipoDeContas)
-router.post('/:_id', controller.addConta)
-router.get('/:_id', controller.getAllContas)
-router.delete('/:_id', controller.remove)
+router.get('/:tipoDeConta', autenticar, controller.tipoDeContas)
+router.post('/:_id', autenticar, controller.addConta)
+router.get('/:_id', autenticar, controller.getAllContas)
+router.delete('/:_id', autenticar, controller.remove)
 
 
 
--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -1,13 +1,35 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/usuarioController')
+const jwt = require('jsonwebtoken')
+const SECRET = process.env.SECRET
+
+
+const autenticar = (req, res, next) => {
+    const authHeader = req.get('authorization')
+
+    if(!authHeader){ return res.status(401).send('cadê os heards anjo')}
+  
+    const token = authHeader.split(' ')[1]
+  
+  
+    jwt.verify(token, SECRET, function(erro){
+       
+      if(erro){
+        
+       return res.status(403).send('foi não visse? tem algo errado')
+      }
+
+  next()
+})
+}
 
 router.post('/cadastro', controller.create)
 router.post('/login', controller.login)
 router.get('/', controller.getAll)
-router.get('tudo/:_id', controller.getById)
-router.delete('/:_id', controller.remove)
-router.patch('/:_id', controller.updateUsuario)
+router.get('tudo/:_id', autenticar, controller.getById)
+router.delete('/:_id', autenticar, controller.remove)
+router.patch('/:_id', autenticar, controller.updateUsuario)
 
 
 module.exports = router
